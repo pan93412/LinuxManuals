b1ce03e52c42ad05275656510af32fcaaf99c468
pan93412 <pan93412@gmail.com>
2018-05-08 16:58:47 +0800

    增加 bash-it 指令稿。

---
+++ ArchLinux/Extras/bash-it.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+function help {
+    echo "      BASH IT 安裝程序"
+    echo " * 安裝說明 * "
+    echo " 感謝您使用本 BASH 檔案安裝 "
+    echo " BASH IT！Bash it 是個 BASH"
+    echo " 的社區維護的 Bash 擴充，包含了"
+    echo " 主題等功能。"
+    echo " 使用：$0 [install | remove]"
+    }
+
+function install {
+    echo "      * 開始安裝 BASH IT *"
+    echo "[正在進行] 正在安裝 git 與 bash ..."
+    sudo pacman -Sy --needed --noconfirm git bash
+    echo "[正在進行] 正在設定預設終端器…"
+    sudo chsh -s /bin/bash
+    echo "[正在進行] 正在下載 BASH IT ..."
+    rm -rf ~/.bash_it
+    git clone --depth=1 https://github.com/Bash-it/bash-it ~/.bash_it
+    echo "[正在進行] 正在安裝 BASH IT ..."
+    bash ~/.bash_it/install.sh -s
+    echo "[即將完成] 正在設定主題至 powerline-plain ..."
+    sed -i "s/^export BASH_IT_THEME='.*'/export BASH_IT_THEME='powerline'/g" ~/.bashrc
+    git clone --depth=1 https://github.com/powerline/fonts.git ~/.powerline-fonts
+    bash ~/.powerline-fonts/install.sh
+    echo "[結束] 正在結束…"
+    bash
+    exit
+}
+
+function remove {
+    echo "      * 開始移除 BASH IT *"
+    echo "[正在進行] 正在移除 BASH IT ..."
+    bash ~/.bash_it/uninstall.sh
+    rm -rf ~/.bash_it
+    echo "[正在進行] 正在移除主題字體 ..."
+    bash ~/.powerline-fonts/uninstall.sh
+    rm -rf ~/.powerline-fonts
+    echo "[結束] 正在結束…"
+    bash
+    exit
+}
+
+case $1 in
+install)
+    install
+    ;;
+remove)
+    remove
+    ;;
+*)
+    help
+    ;;
+esac
--- ArchLinux/Extras/zshinst.sh
+++ ArchLinux/Extras/zshinst.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 # ZSH AUTOMATIC INSTALL for ARCH LINUX
-# usage: zshinst
+# usage: zshinst [--help] (--no-themes)
 # AUTHOR: byStarTW
 
 function install {
-    echo "開始安裝 zsh 與 git 元件 ..."
-    sudo pacman -Sy --noconfirm --needed zsh git
-    echo "設定 zsh 為預設值"
-    sudo chsh -s /usr/bin/zsh
+    echo "開始安裝 zsh 與 git、curl 元件 ..."
+    sudo pacman -Sy --noconfirm --needed zsh git curl
     echo "開始安裝 oh-my-zsh 元件…"
     curl -L http://install.ohmyz.sh >ohmyz.sh
     sed -i 's/env zsh//g' ohmyz.sh
     bash ohmyz.sh
-    echo "設定主題為 agnoster ..."
-    sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="agnoster"/g' ~/.zshrc
-    echo "完成。請自行重新啟動系統！"
+    if [ ! "$1" == "--no-themes" ]; then
+        echo "設定主題為 agnoster ..."
+        sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="agnoster"/g' ~/.zshrc
+    fi
+    echo "完成。"
 }
 
-install
+function usage1 {
+    echo "可用指令：zshinst [--help] (--no-themes)"
+    echo "zshinst 直接執行就是安裝 zsh。"
+    echo "增加 --no-themes 則不將主題設定為 agnoster"
+    echo "--help：說明"
+}
+
+if [ "$1" == "--help" ]; then
+    usage1
+    exit
+fi
+
+install $1
 exit
